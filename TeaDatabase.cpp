b590fad28a9d4b81f192cc08d5a0fe9186e39d2c
CPowerMav <62214093+CPowerMav@users.noreply.github.com>
2023-12-02 13:22:32 -0500

    Add teas and correct params

--- TeaDatabase.cpp
+++ TeaDatabase.cpp
@@ -2,7 +2,10 @@
 #include "TeaDatabase.h"
 
 const TeaType teaTypes[] = {
-    {"White Tea", 270, 79},
-    {"Green Tea", 210, 79},
+    {"White Tea", 270000, 79},
+    {"Green Tea", 240000, 79},
+    {"Black Tea", 210000, 91},
+    {"Oolong Tea", 210000, 91},
+    {"Herbal Tea", 800000, 99},
     // Add more tea types as needed
 };
